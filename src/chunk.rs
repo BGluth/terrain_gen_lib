59673867f465cde83a528e0a780bf3fcf81ef172
BGluth <gluthb@gmail.com>
2023-06-08 18:38:29 -0500

    Some initial planning for chunk coords

---
+++ src/chunk.rs
@@ -0,0 +1,32 @@
+use std::hash::Hash;
+
+pub trait ChunkCoord: Eq + Hash + Sized {
+    fn get_neighboring_chunk_coords_in_radius(&self, r: usize) -> impl Iterator<Item = Self>;
+
+    // Might end up not using a float...
+    fn scale_coord(&self, factor: f32) -> Self;
+}
+
+/// Get the super chunk coordinate that this sub-chunk belongs to.
+///
+/// For example, if we have the coord `(3,3)` at 2x and we want to know the 3x coordiate that it is child of, we would get back `(2,2)`.
+pub(crate) fn get_super_chunk_coord_of_coord<T: ChunkCoord>(
+    coord: &T,
+    coord_scale_factor: usize,
+    target_coord_scale_factor: usize,
+) -> T {
+    debug_assert!(coord_scale_factor <= target_coord_scale_factor);
+
+    todo!()
+}
+
+pub(crate) fn get_sub_chunk_coord_of_coord<T: ChunkCoord>(
+    coord: &T,
+    coord_scale_factor: usize,
+    target_coord_scale_factor: usize,
+) -> impl Iterator<Item = T> {
+    debug_assert!(coord_scale_factor >= target_coord_scale_factor);
+
+    // TODO
+    std::iter::empty()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-//#![allow(warnings)]
+#![feature(return_position_impl_trait_in_trait)]
 
 pub mod GeneratorState;
+pub mod chunk;
 pub mod generation;
