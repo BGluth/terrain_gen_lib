ea9b8c0051b12dc999bde8a3e6257179e0bea353
BGluth <gluthb@gmail.com>
2023-06-11 11:51:05 -0500

    Renamed `terrain_generation.rs` to `generation_state.rs`
    - Just seemed like a better name.
    - Will remove `GenerationState.rs` once the refactor is fully done.

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@
 
 pub mod GeneratorState;
 pub mod chunk;
+pub mod generation_state;
 // pub mod generation;
 pub mod pass_dependencies;
 pub mod pass_params;
 pub mod scheduler_pass_data;
-pub mod terrain_generation;
 pub mod utils;
